a9d6ccb3aa8ada55e8acd2d2ba4c273ef97223f0
System administrator <root@vulcan.local>
2025-06-11 13:26:17 -0700

    changes

--- configuration.nix
+++ configuration.nix
@@ -362,6 +362,19 @@ rec {
       '';
     };
 
+    fail2ban = {
+      enable = true;
+
+      jails.sshd.settings = {
+        enabled = true;
+        maxretry = 10;        # Allow up to 10 failed attempts
+        findtime = 3600;      # Count failures within an hour (3600 seconds)
+        bantime = "24h";      # Ban for one day
+        backend = "systemd";  # Use systemd journal (works best on NixOS)
+      };
+    };
+
+
     pihole-ftl = {
       enable = true;
       openFirewallDHCP = true;

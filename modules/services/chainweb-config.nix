29bcd2826b82048f3aff61066012afb521637975
John Wiegley <johnw@newartisans.com>
2025-10-01 14:22:56 -0700

    chainweb-config: Add testnet04 and evm-testnet nodes
    * modules/services/chainweb-config.nix:

--- modules/services/chainweb-config.nix
+++ modules/services/chainweb-config.nix
@@ -8,14 +8,24 @@
 
     # Configure multiple chainweb nodes to monitor
     nodes = {
-      mainnet01-production = {
+      mainnet01 = {
         apiUrl = "https://api.chainweb.com/chainweb/0.0/mainnet01/cut";
         port = 9101;
       };
 
-      mainnet01-local = {
+      mainnet01-athena = {
         apiUrl = "http://athena.lan:1848/chainweb/0.0/mainnet01/cut";
         port = 9102;
+      };
+
+      testnet04 = {
+        apiUrl = "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/cut";
+        port = 9103;
+      };
+
+      evm-testnet = {
+        apiUrl = "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/cut";
+        port = 9104;
       };
     };
   };

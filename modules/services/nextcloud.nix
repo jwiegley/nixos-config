a8b3e1a8945e35e2ce6877f564d3f12ecf4db15c
John Wiegley <johnw@newartisans.com>
2025-10-02 10:53:13 -0700

    nextcloud: Upgrade to Nextcloud 31
    - **modules/services/nextcloud.nix**: Update package version from nextcloud30
      to nextcloud31

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -26,7 +26,7 @@
   # Nextcloud service configuration
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = "nextcloud.vulcan.lan";
     https = true;
 

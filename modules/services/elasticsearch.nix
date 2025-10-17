01db49e1679313df2ea37568dee5c99e5f6a7607
John Wiegley <johnw@newartisans.com>
2025-10-17 00:14:31 -0700

    Remove unused Elasticsearch module import
    - **hosts/vulcan/default.nix**: Remove import of elasticsearch.nix module
      which was only disabling the service
    
    - **modules/services/elasticsearch.nix**: Delete unused module that disabled
      Elasticsearch 7, as ES 8+ is provided via container (elasticsearch-quadlet.nix)

--- modules/services/elasticsearch.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  # Elasticsearch 7 is not used - RAGFlow requires ES 8+
-  # See modules/containers/elasticsearch-quadlet.nix for ES 8 container
-  services.elasticsearch.enable = false;
-}

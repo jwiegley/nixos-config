ce5a25d46e6f981d4776dde486817418a0fb67ef
John Wiegley <johnw@newartisans.com>
2025-10-17 00:22:14 -0700

    programs: Configure git to rewrite SSH URLs to HTTPS
    - **modules/core/programs.nix**: Expand git configuration to rewrite SSH
      GitHub URLs to HTTPS, enabling git-workspace to work with GITHUB_TOKEN
      authentication for repositories with SSH submodule URLs

--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -2,7 +2,18 @@
 
 {
   programs = {
-    git.enable = true;
+    git = {
+      enable = true;
+      config = {
+        # Rewrite SSH URLs to HTTPS URLs for GitHub
+        # This allows git-workspace to work with GITHUB_TOKEN authentication
+        # even when repositories have submodules with SSH URLs
+        url."https://github.com/".insteadOf = [
+          "git@github.com:"
+          "ssh://git@github.com/"
+        ];
+      };
+    };
     htop.enable = true;
     tmux.enable = true;
     vim.enable = true;

722040cafd0256957e41cfff64bb315fe8a770c4
John Wiegley <johnw@newartisans.com>
2025-10-21 15:05:05 -0700

    system: Remove console blanking service
    - **modules/core/system.nix**: Remove systemd service that configures console
      blanking and powerdown on multiple TTYs via setterm

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -35,20 +35,4 @@
       generateKey = true;
     };
   };
-
-  # Enable console blanking and powerdown
-  systemd.services.console-blanking = {
-    description = "Enable console blanking and powerdown on all TTYs";
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      for tty in tty1 tty2 tty3 tty4 tty5 tty6; do
-        ${pkgs.util-linux}/bin/setterm --blank 1 --powerdown 2 --term linux > /dev/$tty 2>/dev/null || true
-      done
-    '';
-  };
 }

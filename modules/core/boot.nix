3052df1d52931bc7ab36b52f7aac9fee29eed68e
John Wiegley <johnw@newartisans.com>
2025-10-27 11:49:19 -0700

    boot: Reorganize and expand ZFS configuration
    - **modules/core/boot.nix**: Move ZFS filesystem support and pool configuration
      to dedicated zfs.nix module; improve comment formatting for QEMU binfmt
      emulation setup
    
    - **modules/storage/zfs.nix**: Restructure boot configuration using nested
      attribute set for clearer organization; add "gdrive" pool to supported
      extraPools and autoScrub targets; add gdrive sanoid template configuration
      with archival template and recursive snapshots enabled

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -7,16 +7,17 @@
       efi.canTouchEfiVariables = false;
     };
 
-    supportedFilesystems = [ "zfs" ];
-    zfs.extraPools = [ "tank" ];
-
     # Enable QEMU user-mode emulation for running amd64 containers on ARM64
     # Note: preferStaticEmulators causes build failures on ARM64
-    # Using default (non-static) emulator configuration with wrapper for container support
+    # Using default (non-static) emulator configuration with wrapper for
+    # container support
     binfmt = {
       emulatedSystems = [ "x86_64-linux" ];
-      # Don't override fixBinary - let it default to false for non-static emulators
-      # This allows the QEMU wrapper (wrapQemuBinfmtP) to work correctly in containers
+
+      # Don't override fixBinary - let it default to false for non-static
+      # emulators
+      # This allows the QEMU wrapper (wrapQemuBinfmtP) to work correctly in
+      # containers
     };
   };
 

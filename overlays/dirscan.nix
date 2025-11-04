a47579f554d9c36c36672a300c15818a83579aa8
John Wiegley <johnw@newartisans.com>
2025-11-03 18:02:48 -0800

    Update dirscan Nix overlay to use the latest upstream revision
    - **overlays/dirscan.nix**: Updated GitHub revision from
      `c4f4823b1847ae6d1e2438d56677851e13de79fb` to
      `73378768709014a2ac97171c36a2287da79d09aa` with corresponding sha256 hash
      update, and added installation of `share.py` to `$out/bin` directory
      alongside the existing `cleanup` script.

--- overlays/dirscan.nix
+++ overlays/dirscan.nix
@@ -8,9 +8,9 @@ dirscan = with super; python3Packages.buildPythonPackage rec {
   src = super.fetchFromGitHub {
     owner = "jwiegley";
     repo = "dirscan";
-    rev = "c4f4823b1847ae6d1e2438d56677851e13de79fb";
-    sha256 = "1zv1rrbcsi453bwp83xv6ldzsv149x5s1ankm8wd5bqfbq9gywsj";
-    # date = "2025-11-02T23:43:30-08:00";
+    rev = "73378768709014a2ac97171c36a2287da79d09aa";
+    sha256 = "02a1pr9msnxaydzdx7j7bnis54ylpccz9akbyri8yx2rgzgyvgm5";
+    # date = "2025-11-03T17:26:54-08:00";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
@@ -20,6 +20,7 @@ dirscan = with super; python3Packages.buildPythonPackage rec {
     cp dirscan.py $out/libexec
     python -mpy_compile $out/libexec/dirscan.py
     cp cleanup $out/bin
+    cp share.py $out/bin
   '';
 
   meta = {

0f6075b0ee38e446eeb1ba971fae0256a569cff7
John Wiegley <johnw@newartisans.com>
2025-11-08 23:29:29 -0800

    copyparty, nasimw: Add user, copyparty shares
    - **hosts/vulcan/default.nix**: Import nasimw user module to enable account on
      vulcan host
    
    - **modules/containers/secure-nginx.nix**: Add conditional nasimw password file
      handling to copyparty-password-setup service using lib.optionalString to copy
      and chmod password file only when secret exists; declare
      copyparty/nasimw-password SOPS secret with service restart; create
      /tank/Public/johnw and /tank/Public/nasimw directories via tmpfiles.rules;
      add nasimw to copyparty accounts map; define /johnw share with rwmda access
      for johnw user and /nasimw share with rwmda access for both nasimw and johnw
      users, both shares using nodupe, e2d, and d2t flags
    
    - **modules/services/copyparty.nix**: Add nasimwPasswordLine variable with
      conditional logic to read password from passwordFiles.nasimw if using file
      mode or copyparty/nasimw-password SOPS secret if using SOPS mode; inject
      NASIMW_PASS environment variable into configGenerator script for both SOPS
      and file-based password modes; add nasimw account entry to accounts section
      only when password exists via conditional template substitution
    
    - **modules/users/nasimw.nix**: Create nasimw user module with gid 991 group
      and uid 1001 normal user account for "Nasim Wiegley" with bash shell and home
      directory /home/nasimw

---
+++ modules/users/nasimw.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  users = {
+    groups.nasimw = {
+      gid = 991;
+    };
+
+    users.nasimw = {
+      uid = 1001;
+      isNormalUser = true;
+      description = "Nasim Wiegley";
+      group = "nasimw";
+      extraGroups = [ ];
+      home = "/home/nasimw";
+      shell = pkgs.bash;
+      packages = with pkgs; [];
+    };
+  };
+}
